8f03ec2569af09a40070bc9b03a8e8b6a1da17df
g0th2000 <git@gothmog.org.uk>
2024-02-04 23:10:27 +0000

    Tabs = 4 spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,9 +9,9 @@ require("config.vim-fugitive")
 --   { filetypes = { "c", "cpp", "opencl" } },
 -- })
 
--- vim.opt.tabstop = 4
--- vim.opt.softtabstop = 4
--- vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- vim.g["airline_statusline_ontop"] = 1
 vim.g.autoformat = false
